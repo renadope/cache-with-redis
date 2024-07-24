779788d54deee831689180b2f546b35d3f263822
renadope <renadoparia@gmail.com>
2024-07-24 15:27:20 -0400

    add application struct

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "log"
+import (
+	"github.com/redis/go-redis/v9"
+	"log"
+)
+
+type application struct {
+	redisConn *redis.Client
+}
 
 func main() {
 	log.Println("hello world")
