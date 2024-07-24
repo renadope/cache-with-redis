f01fc8f488fe5d07e38a83693d008310dce0d4fd
renadope <renadoparia@gmail.com>
2024-07-24 16:05:26 -0400

    added redis connection

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"log/slog"
 	"os"
+	redis_helpers "redislayer/internal/redis-helpers"
+	"time"
 )
 
 type application struct {
@@ -18,4 +20,31 @@ func main() {
 	app.Logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}))
+	redisConn, err := redis_helpers.NewRedisClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+
+		MaxRetries:      5,
+		MinRetryBackoff: 250 * time.Millisecond,
+		MaxRetryBackoff: 1 * time.Second,
+
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+		PoolTimeout:  4 * time.Second,
+
+		PoolSize:     100,
+		MinIdleConns: 20,
+	}, app.Logger)
+	if err != nil || redisConn == nil {
+		log.Fatalf("failed to connect to redis")
+	}
+	defer func(redisConn *redis.Client) {
+		closeErrRedis := redisConn.Close()
+		if closeErrRedis != nil {
+			log.Fatalf("error in closing redis connection:%v", closeErrRedis)
+		}
+	}(redisConn)
+	app.RedisConn = redisConn
 }
